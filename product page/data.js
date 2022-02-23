63c05e3acd04400e49bc78e36ae684cf2621247a
Tapish sharma <tapishsharma1234@gmail.com>
2022-02-23 08:34:25 +0530

    Database created
    30 items
    10 Pendant
    10 Chandeliers
    10 Flush Mount
    Each item has three categories (Material)
    1. Glass
    2. Wood
    3.Metal

---
+++ product page/data.js
@@ -0,0 +1,201 @@
+var dataLights = [
+  //Pendant
+  //Glass
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/c206a6e24c9d1ab0cac54cd09e78213107cdccfd/Abalone-Glass-Pendant-Light-by-Hampton-Hill.jpg?imwidth=480&impolicy=medium",
+    Sale: 7785,
+    name: "Abalone Glass Pendant Light by Hampton Hill",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/ba16879f61f0bb9bfcf3457a4f1b48acfc2da19d/CARRO-Libra-Sphere-1---Light-Single-Glass-Pendant-Light-Clear.jpg?imwidth=480&impolicy=medium",
+    Sale: 8047,
+    name: "CARRO Libra Sphere 1 - Single Glass Pendant Light-Clear",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/4d515ccd1ed417ba78fa8434b8a78ce4e198b196/Vintage-Pendant-Lighting%2C-Adjustable-Farmhouse-Ceiling-Light-with-Glass-Shade-for-Restaurant%2C-Hallway%2C-Kitchen-Island.jpg?imwidth=480&impolicy=medium",
+    Sale: 8516,
+    name: "Vintage Pendant light with Glass",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/19512234/Beveen-Round-3-Light-Glass-Gourde-Lamp-Collection-2b6f2744-8d09-4b19-b164-4fd94c02b5e3_600.jpg?imwidth=480&impolicy=medium",
+    Sale: 10005,
+    name: "Beveen Round 3-Light Glass Pendant Groude Lamp Collection",
+  },
+
+  //Pendant //wooden
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/3dc277f0c44f681004dc6638f02e40f44a16f587/Wisteria-Distressed-Globe-Wooden-Pendant-1-Light-Farmhouse-Fixture.jpg?imwidth=480&impolicy=medium",
+    Sale: 7155,
+    name: "Wisteria Wooden Pendant Light - Distressed Grey Wood",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/55562b2b758466a0b6b68900509634625ab004d4/Cane-Ribs-Rattan-Hanging-Lamp.jpg?imwidth=480&impolicy=medium",
+    Sale: 37878,
+    name: "Cane Ribs Wooden Pendant Hanging Lamp",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/17018092/Woodbridge-Lighting-17220CBR-WLK3WN-Walden-Wood-Pendant-09f0b96f-c371-47f4-85bf-36f5579bb560_600.jpg?imwidth=480&impolicy=medium",
+    Sale: 15044,
+    name: "Woodbridge Lighting Walden Wood Pendant",
+  },
+
+  //pendant //metal
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/30160793/Black-Metal-LED-Lighting-With-Gold-Metal-Hardware-c900d1e8-95e3-4398-9c27-73f24982c723_600.jpg?imwidth=480&impolicy=medium",
+    Sale: 16974,
+    name: "Black Light LED Gold Metal Pendant",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/57a0229ca2c7b3e695450e08ff74c5521d5671eb/Order-Desk.jpg?imwidth=480&impolicy=medium",
+    Sale: 5230,
+    name: "Carbon Loft Kamari mini Metal Pendant Light",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/14c7fa97656b349fb4a15ed6a1e9c9ee1e86b33d/Pagoda-16%22-4-Bulb-Lantern-Metal-LED-Pendant-by-JONATHAN-Y.jpg?imwidth=480&impolicy=medium",
+    Sale: 11416,
+    name: "Pagoda Bulb LED Metal Pendant by Jonathan",
+  },
+
+  //Chandeliers //glass
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/9a68e06c10d252aadbc6c4b5c391a66b81876655/6-light-Modern-Linear-37.5%27%27-Large-Chandeliers-Glass-Shade-Islands-Lights.jpg?imwidth=480&impolicy=medium",
+    Sale: 27299,
+    name: "6-Light Mordern Glass Chandeliers",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/fc532b1dbaf9e6947e2a3564c6e42c284b2dabdb/3-Light-Modern-Gold-Crystal-Chandelier.jpg?imwidth=480&impolicy=medium",
+    Sale: 30482,
+    name: "Modern glam-3 Glass Chandeliers",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/821770a7f4b6642d6654c869d2262ae4b111e426/Traditional-Crystal-6-Light-Crystal-Chrome-Chandelier.jpg?imwidth=480&impolicy=medium",
+    Sale: 37601,
+    name: "Traditional Crystal Chrome Glass Chandeliers",
+  },
+
+  //Wood
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/5606b6ae61550f93cd404dc2a8999ba2787f78b6/Boho-Gold-4-light-Black-Wood-Beads-Chandelier-Living--Dining-Room-Pendant.jpg?imwidth=480&impolicy=medium",
+    Sale: 21108,
+    name: "Boho Gold Black Wooden Chandeliers",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/abdbd667783e6fa3b125fbd96daf074f7ff4ced3/Modern-Farmhouse-4-light-Distressed-Wood-Chandelier-Island-Lights.jpg?imwidth=480&impolicy=medium",
+    Sale: 28688,
+    name: "Mordern Farmhouse Wooden Chandeliers",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/e04144466af9cafe2efdb507505b0a35e2f2a9d8/Carbon-Loft-Farmhouse-6-light-Wood-Wagon-Wheel-Chandelier-with-Glass-Shade-for-Dining-Room.jpg?imwidth=480&impolicy=medium",
+    Sale: 25532,
+    name: "Carbom Loft Farmhouse Wooden Chandeliers",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/72eaeb283da69ace580179ce223949c011e94a5b/Modern-Farmhouse-Gold-Glam-3-light-Wood-Chandelier-for-Kitchen-Dining-Room.jpg?imwidth=480&impolicy=medium",
+    Sale: 29173,
+    name: "Mordern Gold Glam Wooden Chandeliers",
+  },
+
+  //Metal
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/a41b31204a3c59bbaa3c3f7a42de4360cf54f6e0/Rustic-Antique-Gold-Drum-Chandelier-Linear-Pendant-Lighting.jpg?imwidth=480&impolicy=medium",
+    Sale: 22042,
+    name: "Mid-century Mordern France Metal Chandeliers",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/4d41b2fa2a365f1cbdf99c1e94cc4ac3ebf4ff7a/22-Inch-Gold-Metal-Chandelier-With-Clear-Crystals.jpg?imwidth=480&impolicy=medium",
+    Sale: 50206,
+    name: "Gold Metal Chandeliers with Clear Crystal",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/fc1366553c60514ab04d1e0b661405d9a7125927/Clarisse-24-Inch-Chrome-Polished-Finish-Ceiling-Lighted-Chandelier-Modern-Contemporary-Style-9-Light.jpg?imwidth=480&impolicy=medium",
+    Sale: 23085,
+    name: "Clarisse Chrome Polished Finish Ceiling Metal Chandeliers",
+  },
+
+  //Flush mount lights
+  //Glass
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/421a783b6fb8355398ea4f09d2056c28ab415ca0/Stella-9.75%22-Moravian-Star-Metal-Glass-LED-Pendant%2C-Oil-Rubbed-Bronze-by-JONATHAN-Y.jpg?imwidth=480&impolicy=medium",
+    Sale: 9561,
+    name: "Moravian Star Glass Flush Mount Light",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/89d364152649c766c72cba5b7bb3960c5387d78a/Modern-Glass-Water-Ripple-3-light-Full-Flush-Mount-Ceiling-Light.jpg?imwidth=480&impolicy=medium",
+    Sale: 16765,
+    name: "Mordern Glass Water Ripple Flush Mount Light",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/fe76f004fde033d0a0e206451a20b358289539ef/Mariana-3-Light-Antique-Black-Flush-Mount-with-Cognac-Glass-Shade.jpg?imwidth=480&impolicy=medium",
+    Sale: 11564,
+    name: "Marirana Antique Black Glass Flush Mount Light Cognac Shade",
+  },
+
+  //Wood
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/c352db8f7217cad2b78a7a0266ce9c4746ce0716/White-Wood-Bead-Chandelier-3-Light-Flush-Mount-Ceiling-Light-Fixture.jpg?imwidth=480&impolicy=medium",
+    Sale: 11206,
+    name: "White Wood Bead Flush Mount Ceiling Light ",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/83c95e3aa138ab86d3b688ab525a31f59f3fb77f/Zander-3-Light-Flush-Mount---Golden-Bronze-with-Gray-Wood-Beads.jpg?imwidth=480&impolicy=medium",
+    Sale: 14568,
+    name: "Zander Golden Bronze Wood Bead Flush Mount Light ",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/29958197/Bernice-4-Light-Black-Metal-and-Natural-Wood-Flush-Mount-d950bcdf-8acb-4cf2-9035-af0d0a1f3eea_600.jpg?imwidth=480&impolicy=medium",
+    Sale: 7799,
+    name: "Bernice Natural Wood Flush Mount Light ",
+  },
+
+  //Metal
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/c2c3b6ad01ddd27b0564de1f64113255dbff73c9/CO-Z-15-inch-Industrial-3-light-Metal-Cage-Ceiling-Flush-Mount.jpg?imwidth=480&impolicy=medium",
+    Sale: 8068,
+    name: "CO-Z Industrial Metal Cage Flush Mount Light ",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/9f5c38d5d71c17e3545d41e033976d2c21b8b1a4/GetLedel-11-inch-Industrial-Farmhouse-Cage-Flush-Mount-Ceiling-Light.jpg?imwidth=480&impolicy=medium",
+    Sale: 6095,
+    name: "GetLedel Industrial Metal Cage Flush Mount Light ",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/2bd4cb2ef537198543ea879816c45a2ec3830070/13%22-Round-Iron-Flush-Mount-Ceiling-Light.jpg?imwidth=480&impolicy=medium",
+    Sale: 8382,
+    name: "Round Iron Cage Metal Flush Mount Light ",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/5cdd1ff4f744b0d6b79c8d3c18be7c718abc7120/Modern-Farmhouse-3-light-Seeded-Glass-Drum-Chandelier-Semi-flush-Mount-Lights.jpg?imwidth=480&impolicy=medium",
+    Sale: 16765,
+    name: "Mordern Farmhouse Metal Cage Flush Mount Light ",
+  },
+];
